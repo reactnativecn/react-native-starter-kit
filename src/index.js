e160e84364296b1a69dbe9a8fd102e0ea72ff867
DengYun <tdzl2003@gmail.com>
2015-12-20 23:38:17 +0800

    use general entry file.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Yun on 2015-12-20.
+ */
+
+import React, {
+  AppRegistry,
+} from 'react-native';
+
+class App extends React.Component {
+  render(){
+    return (
+      <View />
+    );
+  }
+}
+
+AppRegistry.registerComponent('MyProject', () => App);
